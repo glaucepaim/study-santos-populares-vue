aecce3f80a8e64d9e032ac2209c6fd987faafede
glaucepaim <glaucepaim@gmail.com>
2026-06-20 23:50:59 +0100

    feat: adicionar Hero section com countdown para São João

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <main>
+      <HeroSection />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/layout/AppHeader.vue'
+import AppFooter from './components/layout/AppFooter.vue'
+import HeroSection from './components/home/HeroSection.vue'
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+@import './assets/styles/variables.css';
+@import './assets/styles/base.css';
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+}
+</style>
